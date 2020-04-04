83ed2b7649f86b920c3cffb9c31766f8391df484
lachoneus <6295215+lachoneus@users.noreply.github.com>
2020-04-04 14:27:41 -0600

    Premiliminary support for install on Debian 9 platforms, added menu to select between ubuntu and debian 9

--- rmvlib_install.sh
+++ rmvlib_install.sh
@@ -2,20 +2,34 @@
  
 RED='\033[0;31m'
 NC='\033[0m' # No Color
+
+echo "\nEnter the number for which Linux distribution will you be installing on:\n1) Ubuntu\n2) Debian 9"
  
-#add m68k-mint-atari cross compiler 
-echo "\n${RED}Adding Repository For Cross Compiler Tools${NC}\n"
+ 
+read CHOICE
 
-sudo add-apt-repository -y ppa:vriviere/ppa
-sudo apt-get update -y
+if [ $CHOICE -ne 1 ] && [ $CHOICE -ne 2 ]
+then
+    
+    echo "\nInvalid OS selection, exiting...\n"
+    exit 1
+    
+fi
+ 
+if [ $CHOICE = 1 ]
+then
 
-echo  "\n${RED}Installing Cross Compiler Tools & GIT${NC}\n"
+    #add m68k-mint-atari cross compiler from ppa for Ubuntu installs
+    echo "\n${RED}Adding Repository For Cross Compiler Tools for Ubuntu Installation${NC}\n"
+    sudo add-apt-repository -y ppa:vriviere/ppa
+    sudo apt-get update -y
+    echo  "\n${RED}Installing Cross Compiler Tools & GIT${NC}\n"
+    sudo apt-get install -y gcc-m68k-atari-mint
+    sudo apt-get install -y cross-mint-essential
+    
+fi
 
-sudo apt-get install -y gcc-m68k-atari-mint
-sudo apt-get install -y cross-mint-essential
-sudo apt-get install -y git
-sudo apt-get install -y make
-sudo apt-get install -y build-essential
+sudo apt install -y git make build-essential
 
 #setup our folders for our tools and build environment path
 echo  "\n${RED}Adding Tools Directory${NC}\n"
@@ -23,6 +37,8 @@ echo  "\n${RED}Adding Tools Directory${NC}\n"
 mkdir -v $HOME/Jaguar
 mkdir -v $HOME/Jaguar/example_programs
 mkdir -v $HOME/Jaguar/lib
+mkdir -v $HOME/Jaguar/lib/lib
+mkdir -v $HOME/Jaguar/lib/include
 mkdir -v $HOME/Jaguar/src
 
 echo  "\n${RED}Copy Temporary Assets Directory${NC}\n"
@@ -37,9 +53,29 @@ echo  "\n${RED}Setting Environment Variable JAGPATH${NC}\n"
 export JAGPATH=/home/$USER/Jaguar #export for the session the script is running so libraries install correctly
 
 echo "export JAGPATH=/home/$USER/Jaguar" | sudo tee -a /etc/environment 
+ 
+if [ $CHOICE = 2 ]
+then
 
-#download tools
-echo  "\n${RED}Downloading Tools From GIT Repositories${NC}\n"
+    #download tools
+    echo  "\n${RED}Downloading, extracting, and linking m68k cross-compiler toolchain for Debian 9 Installation${NC}\n"
+
+    wget https://tho-otto.de/download/mint/binutils-2.33.1-mint-20200102-bin-linux.tar.xz
+    wget https://tho-otto.de/download/mint/gcc-4.6.4-mint-20170518-bin-linux.tar.xz
+
+    tar xvf binutils-2.33.1-mint-20200102-bin-linux.tar.xz
+    tar xvf gcc-4.6.4-mint-20170518-bin-linux.tar.xz
+
+    sudo ln -sf $HOME/Jaguar/usr/bin/m68k-atari-mint-gcc /usr/bin/m68k-atari-mint-gcc
+    sudo ln -sf $HOME/Jaguar/usr/bin/m68k-atari-mint-ar /usr/bin/m68k-atari-mint-ar
+    cp $HOME/Jaguar/usr/lib64/gcc/m68k-atari-mint/4.6.4/libgcc.a $HOME/Jaguar/lib/lib/libgcc.a
+
+    rm binutils-2.33.1-mint-20200102-bin-linux.tar.xz
+    rm gcc-4.6.4-mint-20170518-bin-linux.tar.xz
+
+fi
+
+echo  "\n${RED}Downloading RMAC/RLN Source From GIT Repositories${NC}\n"
 
 cd $HOME/Jaguar/src
 
@@ -47,6 +83,7 @@ git clone http://shamusworld.gotdns.org/git/rmac
 git clone http://shamusworld.gotdns.org/git/rln
 # git clone https://github.com/ocaml/ocaml.git
 # git clone https://github.com/sbriais/jlinker.git
+echo  "\n${RED}Downloading jlibc/rmvlib Libraries From GIT Repositories${NC}\n"
 git clone https://github.com/sbriais/jlibc.git
 git clone https://github.com/sbriais/rmvlib.git
 
@@ -138,12 +175,17 @@ make install
 
 cd $HOME/Jaguar/src 
 
-#copy libgcc.a from m68k-mint-atari tools
-echo  "\n${RED}copy libgcc.a from m68k-mint-atari tools into lib folder${NC}\n"
+if [ $CHOICE = 1 ]
+then
 
-cd $HOME/Jaguar/lib/lib
+    #copy libgcc.a from m68k-mint-atari tools
+    echo  "\n${RED}copy libgcc.a from m68k-mint-atari tools into lib folder${NC}\n"
 
-cp -v /usr/lib/gcc/m68k-atari-mint/4.6.4/libgcc.a ./
+    cd $HOME/Jaguar/lib/lib
+
+    cp -v /usr/lib/gcc/m68k-atari-mint/4.6.4/libgcc.a ./
+    
+fi
 
 cd $HOME/Jaguar
 
--- rmvlib_uninstall.sh
+++ rmvlib_uninstall.sh
@@ -11,6 +11,7 @@ echo "\n${RED}Removing JAGPATH Environoment Variable${NC}\n"
 
 sudo sed -i '/Jaguar/d' /etc/environment
 
+# old method of insatll cross compiler tools
 echo "\n${RED}Removing Cross Tools and PPA From source.list${NC}\n"
 
 sudo apt-get remove -y --purge gcc-m68k-atari-mint
@@ -20,6 +21,9 @@ sudo apt-get update -y
 
 echo "\n${RED}Removing Symbolic Links to JAGPTAH/bin Executables${NC}\n"
 
+
+sudo rm -v /usr/bin/m68k-atari-mint-gcc
+sudo rm -v /usr/bin/m68k-atari-mint-ar
 sudo rm -v /usr/bin/jag-image-converter
 sudo rm -v /usr/bin/jcp
 sudo rm -v /usr/bin/virtualjaguar
