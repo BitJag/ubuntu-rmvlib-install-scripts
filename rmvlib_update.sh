63ac5580cb0d8c528eb5e1a44c31fa9ff576af89
lachoneus <6295215+lachoneus@users.noreply.github.com>
2021-06-13 09:37:20 -0600

    Modified rmvlib_update.sh scrip to properly update the tools according to the changes made in other scripts

--- rmvlib_update.sh
+++ rmvlib_update.sh
@@ -4,7 +4,7 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 
-echo "\n${NC}This script removes and then updates a current Removers Library tool chain installed with the rmvlib_install.sh script.\nThis assumes that your toolchain folder is still located in the default location, which is <your home folder>/Jaguar. \n\nThis includes RMAC, RLN, JLIBC, RMVLIB${RED}\n\nDo you you wish to continue?  Type: yes/no:${NC}"
+echo "\n${NC}This script removes and then updates a current Removers Library tool chain installed with the rmvlib_install.sh script.\nThis assumes scripts assumes that your JAGPATH environment variable is properly setup.\n\nThis will remove and update RMAC, RLN, JLIBC, RMVLIB, and the generic example folder.\nThis does not update any applications that were installed with the additional applications script.${RED}\n\nDo you you wish to continue?  Type: yes/no:${NC}"
 
 read CONTINUE
 
@@ -13,32 +13,28 @@ then
 
 echo "${RED}\nRemoving local source folders and lib/include folders for RMAC, RLN, JLIBC, RMVLIB.\n${NC}"
 
-rm -rv $HOME/Jaguar/src/rmac
-rm -rv $HOME/Jaguar/src/rln
-rm -rv $HOME/Jaguar/src/jlibc
-rm -rv $HOME/Jaguar/src/rmvlib
-rm -rv $HOME/Jaguar/lib/lib
-rm -rv $HOME/Jaguar/lib/include
+rm -rvf $JAGPATH/src/rmac
+rm -rvf $JAGPATH/src/rln
+rm -rvf $JAGPATH/src/jlibc
+rm -rvf $JAGPATH/src/rmvlib
+rm -rvf $JAGPATH/lib/lib/*
+rm -rvf $JAGPATH/lib/include/*
+rm -rvf $JAGPATH/bin/mac
+rm -rvf $JAGPATH/bin/aln
+rm -rvf $JAGPATH/example_programs/generic_example
 
 echo  "\n${RED}Copy Temporary Assets Directory${NC}\n"
 
-cp -vr ./assets $HOME/Jaguar
+cp -vr ./assets/generic_example $JAGPATH/example_programs/
 
 echo "${RED}\nDownload latest source code for RMAC, RLN, JLIBC, RMVLIB to local src directory.\n${NC}"
 
-cd $HOME/Jaguar/src
+cd $JAGPATH/src
 
 git clone http://shamusworld.gotdns.org/git/rmac
 git clone http://shamusworld.gotdns.org/git/rln
 git clone https://github.com/sbriais/jlibc.git
 git clone https://github.com/sbriais/rmvlib.git
-
-echo "${RED}\nRun any patches in temp assets direction on sources.\n${NC}"
-
-cd rmac
-#Patch rmac
-patch -p1 -i $HOME/Jaguar/assets/patch_files/9-2-2020_rmac.patch
-cd $HOME/Jaguar/src
 
 echo "${RED}\n\nBegin Building sources\n\n${NC}"
 
@@ -46,43 +42,36 @@ echo "${RED}\n\nBegin Building sources\n\n${NC}"
 echo  "\n${RED}Building RMAC${NC}\n"
 cd rmac
 make
-cd $HOME/Jaguar/src
+cp -vr rmac $JAGPATH/bin/mac #renaming to make more compatible with sebs makefiles
+cd $JAGPATH/src
 
-#patching and building rln
+#build rln
 echo  "\n${RED}Building RLN${NC}\n"
 cd rln
 make
-cd $HOME/Jaguar/src
+cp -vr rln $JAGPATH/bin/aln #renaming to make more compatible with sebs makefiles
+cd $JAGPATH/src
 
-#modify and build jlibc
+#build jlibc
 echo  "\n${RED}Building JLIBC${NC}\n"
 cd jlibc
-sed -i '/MADMAC=/c\MADMAC=$(JAGPATH)/src/rmac/rmac' Makefile.config #change makefile.config to point to our new rmac
-sed -i '/OSUBDIRS=/c\OSUBDIRS=ctype' Makefile #don't build documentation
 make
 make install
-cd $HOME/Jaguar/src
+cd $JAGPATH/src
 
-#modify and build rmvlib
+#build rmvlib
 echo  "\n${RED}Building RMVLIB${NC}\n"
 cd rmvlib
-#change makefiles
-sed -i '/MADMAC=/c\MADMAC=$(JAGPATH)/src/rmac/rmac' Makefile.config #change makefile.config to point to our new rmac
-sed -i '/OSUBDIRS=/c\OSUBDIRS=' Makefile #don't build documentation
 make
 make install
 
-cd $HOME/Jaguar/src 
+cd $JAGPATH/src 
 
 #copy libgcc.a from m68k-mint-atari tools
 echo  "\n${RED}copy libgcc.a from m68k-mint-atari tools into lib folder${NC}\n"
-cd $HOME/Jaguar/lib/lib
-cp -v /usr/lib/gcc/m68k-atari-mint/4.6.4/libgcc.a ./
-cd $HOME/Jaguar
-
-
-echo  "\n${RED}Remove Temporary Assets Directory${NC}\n"
-rm -rv ./assets
+cd $JAGPATH/lib/lib
+find /usr/lib/gcc/m68k-atari-mint/ -type f -name "libgcc.a" -exec cp {} ./ \;
+cd $JAGPATH
 
 echo  "\n\n${RED}Finished updating Removers Library${NC}\n\n"
 
