749a1914316cf9d305ce8094f7f64e022b270c1c
BitJag <6295215+BitJag@users.noreply.github.com>
2025-08-06 11:15:45 -0600

    Added proper removal of jaggd.

--- rmvlib_uninstall.sh
+++ rmvlib_uninstall.sh
@@ -21,16 +21,14 @@ sudo apt-get remove -y --purge gcc-m68k-atari-mint
 sudo add-apt-repository -y --remove ppa:vriviere/ppa
 sudo apt-get update -y
 
-echo "\n${RED}Removing Symbolic Links to JAGPTAH/bin Executables${NC}\n"
+echo "\n${RED}Removing Symbolic Links to JAGPTAH/bin Executables, and rules files for JCP(skunk) and jaggd${NC}\n"
 
 sudo rm -v /usr/bin/bigpemu
 sudo rm -v /usr/bin/jag-image-converter
+sudo rm -v /usr/bin/jaggd
+sudo rm -v /etc/udev/rules.d/65-jaggd.rules
 sudo rm -v /usr/bin/jcp
+sudo rm -v /etc/udev/rules.d/64-skunk.rules
 sudo rm -v /usr/bin/lz77
 
-echo "\n${RED}Removing JCP rules file${NC}\n"
-
-sudo rm -v /etc/udev/rules.d/64-skunk.rules
-
 echo "\n${RED}Uninstall Finished${NC}\n"
-
